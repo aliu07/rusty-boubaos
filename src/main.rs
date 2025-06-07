507ff4f19c3867dd7de4c448e3a7baaa22a9ec19
Alexander Liu <alexander_l_22@hotmail.com>
2025-06-07 13:02:05 -0400

    Add core skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-fn main() {
+use std::{
+    io::{Write, stdin, stdout},
+    process,
+};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Banner
     println!(r#"    ____              __          ____  _____"#);
     println!(r#"   / __ )____  __  __/ /_  ____ _/ __ \/ ___/"#);
@@ -8,4 +13,27 @@ fn main() {
     println!("====================================================");
     println!("Version 1.0 created June 2025");
     println!("Type 'help' to get a list of commands\n");
+
+    let prompt = "$";
+    let mut user_input = String::new();
+    let mut error_code;
+
+    loop {
+        print!("{} ", prompt);
+        stdout().flush()?;
+        stdin().read_line(&mut user_input)?;
+        error_code = parse_input(&user_input);
+
+        if error_code == -1 {
+            process::exit(99);
+        }
+
+        user_input.clear();
+    }
+}
+
+fn parse_input(user_input: &str) -> i32 {
+    // Do something
+    println!("{}", user_input);
+    0
 }
