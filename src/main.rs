7a8acbebce6ed7c3075d2eb9bd9471b718efd083
Alexander Liu <alexander_l_22@hotmail.com>
2025-06-07 12:46:11 -0400

    Add startup banner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    // Banner
+    println!(r#"    ____              __          ____  _____"#);
+    println!(r#"   / __ )____  __  __/ /_  ____ _/ __ \/ ___/"#);
+    println!(r#"  / __  / __ \/ / / / __ \/ __ `/ / / /\__ \ "#);
+    println!(r#" / /_/ / /_/ / /_/ / /_/ / /_/ / /_/ /___/ / "#);
+    println!(r#"/_____/\____/\__,_/_.___/\__,_/\____//____/  "#);
+    println!("====================================================");
+    println!("Version 1.0 created June 2025");
+    println!("Type 'help' to get a list of commands\n");
 }
