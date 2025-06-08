2a968e11796cc967d288f4adb1da767979221ad4
Alexander Liu <alexander_l_22@hotmail.com>
2025-06-08 17:23:05 -0400

    Add batch mode execution

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{Write, stdin, stdout};
+use std::io::{BufRead, Write, stdin, stdout};
 
 use crate::errors::BadCommandError;
 
@@ -8,28 +8,43 @@ mod interpreter;
 mod memory;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Banner
-    println!(r#"    ____              __          ____  _____"#);
-    println!(r#"   / __ )____  __  __/ /_  ____ _/ __ \/ ___/"#);
-    println!(r#"  / __  / __ \/ / / / __ \/ __ `/ / / /\__ \ "#);
-    println!(r#" / /_/ / /_/ / /_/ / /_/ / /_/ / /_/ /___/ / "#);
-    println!(r#"/_____/\____/\__,_/_.___/\__,_/\____//____/  "#);
-    println!("====================================================");
-    println!("Version 1.0 created June 2025");
-    println!("Type 'help' to get a list of commands\n");
+    // True = Interactive mode, False = Batch mode (input piped)
+    let mode_flag = atty::is(atty::Stream::Stdin);
 
-    let prompt = "$";
-    let mut user_input = String::new();
+    if mode_flag {
+        // Banner
+        println!(r#"    ____              __          ____  _____"#);
+        println!(r#"   / __ )____  __  __/ /_  ____ _/ __ \/ ___/"#);
+        println!(r#"  / __  / __ \/ / / / __ \/ __ `/ / / /\__ \ "#);
+        println!(r#" / /_/ / /_/ / /_/ / /_/ / /_/ / /_/ /___/ / "#);
+        println!(r#"/_____/\____/\__,_/_.___/\__,_/\____//____/  "#);
+        println!("====================================================");
+        println!("Version 1.0 created June 2025");
+        println!("Type 'help' to get a list of commands\n");
 
-    loop {
-        print!("{} ", prompt);
-        stdout().flush()?;
-        stdin().read_line(&mut user_input)?;
-        parse_input(&user_input).unwrap_or_else(|err| {
-            eprintln!("{}", err);
-        });
+        let prompt = "$";
+        let mut user_input = String::new();
 
-        user_input.clear();
+        loop {
+            print!("{} ", prompt);
+            stdout().flush()?;
+            stdin().read_line(&mut user_input)?;
+            parse_input(&user_input).unwrap_or_else(|err| {
+                eprintln!("{}", err);
+            });
+
+            user_input.clear();
+        }
+    } else {
+        for line in stdin().lock().lines() {
+            let line = line?;
+
+            parse_input(&line).unwrap_or_else(|err| {
+                eprintln!("{}", err);
+            });
+        }
+
+        Ok(())
     }
 }
 
