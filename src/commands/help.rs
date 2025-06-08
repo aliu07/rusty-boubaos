7c362d527862142b40e7d27588aa897ad7de7c51
Alexander Liu <alexander_l_22@hotmail.com>
2025-06-08 18:11:38 -0400

    Update help string

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -1,14 +1,15 @@
 pub fn help() {
     let help_string = r#"COMMAND          DESCRIPTION
 ====================================================
-help             Displays all the commands
-quit             Exits / terminates the shell with "Bye!"
-set VAR STRING   Assigns a value to shell memory
-remove VAR       Removes VAR from shell memory
-print VAR        Displays the STRING assigned to VAR
-run SCRIPT.TXT   Executes the file SCRIPT.TXT
 echo STRING      Displays STRING. If STRING starts with $, prints
-                 the value of the variable"#;
+                 the value of the variable
+help             Displays all the commands
+print VAR        Displays the STRING assigned to VAR
+pwd              Prints the current working directory path
+quit             Exits / terminates the shell with "Bye!"
+remove VAR       Removes VAR from shell memory
+run SCRIPT.TXT   Executes the text file SCRIPT.TXT
+set VAR STRING   Assigns a value to shell memory"#;
 
     println!("\n{}\n", help_string);
 }
